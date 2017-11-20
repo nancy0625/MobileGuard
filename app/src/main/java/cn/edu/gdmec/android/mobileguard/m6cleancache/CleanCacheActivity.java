92c39df87e6c160e7943d26c88a532a1fc719f48
nancy0625 <1308013168@qq.com>
2017-11-20 15:28:32 +0800

    CacheClearListActivity.java 完成

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/CleanCacheActivity.java
@@ -0,0 +1,21 @@
+package cn.edu.gdmec.android.mobileguard.m6cleancache;
+
+import android.content.DialogInterface;
+import android.view.View;
+
+/**
+ * Created by asus on 2017/11/20.
+ */
+
+public class CleanCacheActivity implements View.OnClickListener {
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/adapter/CacheCleanAdapter.java
@@ -0,0 +1,72 @@
+package cn.edu.gdmec.android.mobileguard.m6cleancache.adapter;
+
+import android.content.Context;
+import android.text.format.Formatter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.edu.gdmec.android.mobileguard.R;
+import cn.edu.gdmec.android.mobileguard.m6cleancache.entity.CacheInfo;
+
+/**
+ * Created by asus on 2017/11/20.
+ */
+
+public class CacheCleanAdapter extends BaseAdapter {
+  private Context context;
+    private List<CacheInfo> cacheInfos;
+    private LayoutInflater inflater;
+
+    public CacheCleanAdapter(Context context,List<CacheInfo> cacheInfos) {
+        this.context = context;
+        this.cacheInfos = cacheInfos;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return cacheInfos.size();
+    }
+
+
+    @Override
+    public Object getItem(int position) {
+        return cacheInfos.get(position);
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if (convertView == null){
+            viewHolder = new ViewHolder();
+            convertView =convertView.inflate(context, R.layout.item_cacheclean_list,null);
+            viewHolder.mAppIconImgv = (ImageView)convertView.findViewById(R.id.imgv_appicon_cacheclean);
+            viewHolder.mAppNameTV = (TextView)convertView.findViewById(R.id.tv_appname_cacheclean);
+            convertView.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        CacheInfo cacheInfo = cacheInfos.get(position);
+        viewHolder.mAppIconImgv.setImageDrawable(cacheInfo.appIcon);
+        viewHolder.mAppNameTV.setText(cacheInfo.appName);
+        viewHolder.mCacheSizeTV.setText(Formatter.formatFileSize(context,cacheInfo.cacheSize));
+        return convertView;
+    }
+    class ViewHolder {
+        ImageView mAppIconImgv;
+        TextView mAppNameTV;
+        TextView mCacheSizeTV;
+    }
+}
